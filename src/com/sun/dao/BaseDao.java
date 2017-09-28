3eaee519fe263a1b12171debe26f6ca71d99d726
Jessie <Jessie>
2017-09-28 23:21:07 +0800

    add mongo db driver

--- src/com/sun/dao/BaseDao.java
+++ src/com/sun/dao/BaseDao.java
@@ -3,10 +3,26 @@ package com.sun.dao;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+
 public abstract class BaseDao {
 	
     @Autowired
     protected SessionFactory sessionFactory;
+    
+    public MongoDatabase getConnection() {
+
+        MongoClientURI uri  = new MongoClientURI("mongodb://user:pass@host:port/db"); 
+        MongoClient client = new MongoClient(uri);
+        MongoDatabase db = client.getDatabase(uri.getDatabase());
+    	
+//    	MongoClient client = new MongoClient( "localhost" , 27017 );
+//    	MongoDatabase db = client.getDatabase("database name");
+//    	boolean auth = db.authenticate("username", "password".toCharArray());
+    	return db;
+    }
     
 
 }
