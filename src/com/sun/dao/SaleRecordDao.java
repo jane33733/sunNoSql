bec4706610aad087019de63ebd11ce81738c1cd7
kitty337733 <kitty33733@gmail.com>
2017-12-03 23:49:18 +0800

    add query group

--- src/com/sun/dao/SaleRecordDao.java
+++ src/com/sun/dao/SaleRecordDao.java
@@ -1,15 +1,21 @@
 package com.sun.dao;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.aggregation.AggregationPipeline;
+import org.mongodb.morphia.aggregation.Group;
+import org.mongodb.morphia.aggregation.Sort;
+import org.mongodb.morphia.query.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.mongodb.WriteResult;
 import com.sun.util.MongoUtil;
 import com.sun.vo.db.SaleRecord;
+import com.sun.vo.transfer.SaleRecordQueryVO;
 
 @Transactional
 @Repository
@@ -63,15 +69,29 @@ public class SaleRecordDao{
         return ( wr.getN()>0 ? true :false );
     }
 	
-	public List<SaleRecord> queryTopAndLast() {
+	public List<SaleRecord> queryTopAndLast(SaleRecordQueryVO queryVO) {
 		// TODO
-		List<SaleRecord> productList = new ArrayList<>();
+		List<SaleRecord> resultList = new ArrayList<>();
 		Datastore dataStore = MongoUtil.getMorphia();
-		productList = dataStore.createQuery(SaleRecord.class).asList();
-//		for (SaleRecord product : productList) {
-//			WriteResult wr = dataStore.delete(product);
-//		}
-		return productList;
+		Query<SaleRecord> query = dataStore.createQuery(SaleRecord.class)
+				.filter("saleDate >= ", queryVO.getStartDate())
+				.filter("saleDate <= ", queryVO.getEndDate())
+				.order("monthSale");
+		
+		
+		AggregationPipeline pipeline = dataStore.createAggregation(SaleRecord.class)
+				.match(query)
+				.group( Group.id( Group.grouping("saleDate"))
+						, Group.grouping("monthSale", Group.sum("Amount"))
+						)
+				;
+		
+		
+		Iterator<SaleRecord> resltIterator = pipeline.aggregate(SaleRecord.class);
+		while (resltIterator.hasNext()) {
+			resultList.add(resltIterator.next());
+		}
+		return resultList;
 	}
 	
 }
---
+++ src/com/sun/vo/transfer/SaleRecordQueryVO.java
@@ -0,0 +1,27 @@
+package com.sun.vo.transfer;
+
+import java.time.LocalDate;
+
+public class SaleRecordQueryVO {
+
+	
+	private LocalDate startDate;
+	private LocalDate endDate;
+	
+	
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+	
+	
+	
+}
